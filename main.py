d246a810d11ef40dee0a619cc30ef5f0d414add6
万袤(wan_m601) <wequart@gmail.com>
2023-12-11 15:20:36 +0800

    feat: 文档注释

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from pydantic import BaseModel
-from fastapi import FastAPI, Path, Body, status, Form, UploadFile, File
+from fastapi import FastAPI, Path, Body, status, Form, UploadFile, File, HTTPException
 from typing import Annotated
 from enum import Enum
 
@@ -48,9 +48,33 @@ class User(BaseModel):
     full_name: str | None = None
 
 
-@app.post("/items", status_code=status.HTTP_201_CREATED)
+@app.post("/items", status_code=status.HTTP_201_CREATED, response_description="创建新的项")
 async def create_item(item: Item):
+    """
+    Create an item with all the information
+    - **name** - the name of the item
+    - **description** - the description of the item
+    - **price** - the price of the item
+    - **tax** - the tax of the item
+    - **tag** - the tag of the item
+    :param item:
+    :return:
+    """
     return {"status": "success", **item.model_dump()}
+
+
+items = {"foo": "the foo wrestlers"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found",
+            headers={"X-Error": "There goes my error"}
+        )
+    return {"item": items.get(item_id)}
 
 
 # query, path, request body 混合传参
@@ -81,12 +105,12 @@ async def login(username: Annotated[str, Form()], password: Annotated[str, Form(
     return {"username": username, "password": password}
 
 
-@app.post("/files/")
+@app.post("/files/", tags=['文件上传'])
 async def create_file(file: Annotated[bytes, File()]):
     return {"file_size": len(file)}
 
 
-@app.post("/uploadfile")
+@app.post("/uploadfile", tags=['文件上传'])
 async def create_upload_file(file: UploadFile):
     return {"file_name": file.filename}
 
