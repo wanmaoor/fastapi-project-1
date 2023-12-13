632a723a6755d2725d3e28776052985af7e5eed8
万袤(wan_m601) <wequart@gmail.com>
2023-12-13 11:36:29 +0800

    feat: 元数据

--- app/main.py
+++ app/main.py
@@ -2,8 +2,53 @@ from fastapi import FastAPI, Depends
 from .dependencies import get_query_token
 from .routers import items
 from .notification import main
+
+description = """
+ChimichangApp API helps you do awesome stuff. 🚀
+
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+
+tags_metadata = [
+    {
+        "name": "通知",
+        "description": "背景任务相关",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
 app = FastAPI(
-    dependencies=[Depends(get_query_token)]
+    title="ChimichangApp",
+    description=description,
+    summary="ChimichangApp API helps",
+    version="0.0.1",
+    terms_of_service="https://www.google.com/policies",
+    contact={
+        "name": "wanmao",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    dependencies=[Depends(get_query_token)],
+    openapi_tags=tags_metadata
 )
 
 app.include_router(items.router)
@@ -13,5 +58,6 @@ app.include_router(main.router)
 @app.get("/")
 async def root():
     return {"message": "Hello Bigger Application"}
-print(f'current name: {__name__}')
 
+
+print(f'current name: {__name__}')
